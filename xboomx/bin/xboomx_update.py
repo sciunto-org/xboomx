53f43df1e6d85512504b38429fc59f931b18b266
François Boulogne <devel@sciunto.org>
2025-02-12 22:24:44 +0100

    no need of arg handling

--- xboomx/bin/xboomx_update.py
+++ xboomx/bin/xboomx_update.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python3
-from pprint import pprint
 import sys
+from pprint import pprint
 from sqlalchemy.orm.exc import NoResultFound
 from xboomx.sqlitemgr import get_session, PathItem
 
 
 def main():
-    # get db type
-    db_type = ''
-    if len(sys.argv) > 1 and sys.argv[1] != "--stats":
-        db_type = sys.argv[1]
-
     item = sys.stdin.read().strip('\n')
 
     # If user pressed Esc
